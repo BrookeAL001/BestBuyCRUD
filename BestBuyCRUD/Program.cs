c4241573f647dc0b052656d696620bfd3f92c63f
BrookeAL0012 <bbcecil17@gmail.com>
2022-07-20 15:17:10 -0500

    Finished project, took some time (yikes)

--- BestBuyCRUD/Program.cs
+++ BestBuyCRUD/Program.cs
@@ -1,36 +1,104 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
-using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
 
 namespace BestBuyCRUD
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-
-            #region Configuration
-            var config = new ConfigurationBuilder()
+        #region Configuration
+        
+        static IConfiguration config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
                 .Build();
 
+        static string connString = config.GetConnectionString("DefaultConnection");
 
-            string connString = config.GetConnectionString("DefaultConnection");
-            #endregion
+        //gives us a connection to the MySQL data
+        static IDbConnection conn = new MySqlConnection(connString);
+        #endregion
 
-            IDbConnection conn = new MySqlConnection(connString);
+
+        static void Main(string[] args)
+        {
+            ListProducts();
+
+            DeleteThisProduct();
+
+            ListProducts();
+
+        }
+
+        public static void DeleteThisProduct()
+        {
+            var prodRepo = new DapperProductRepository(conn);
+
+            Console.WriteLine("Please enter the Product ID of the product you would like to delete");
+            var productID = Convert.ToInt32(Console.ReadLine());
+
+            prodRepo.DeleteProduct(productID);
+        }
+
+        public static void UpdateProductName()
+        {
+            var prodRepo = new DapperProductRepository(conn);
+
+            Console.WriteLine($"What is the Product ID of the product you wish to update?");
+            var productID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"What is the new name you would like for the product with the ID of {productID}");
+            var updatedName = Console.ReadLine();
+
+            prodRepo.UpdateProduct(productID, updatedName);
+        }
+
+ 
+        public static void CreateNewProducts()
+        {
+            var prodRepo = new DapperProductRepository(conn);
+
+            Console.WriteLine($"What is the new product name?");
+            var prodName = Console.ReadLine();
+
+            Console.WriteLine($"What is the price of the new product?");
+            var price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"What is the new product's Category ID (Number should be between 1 and 10)");
+            var categoryID = Convert.ToInt32(Console.ReadLine());
+
+            prodRepo.CreateProduct(prodName, price, categoryID);
+
+            var products = prodRepo.GetAllProducts();
+
+            Console.WriteLine("Updated Product List:");
+            
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID:  {product.ProductID}     Product Name:  {product.Name}");
+            }
+        }
+
+
+ 
+        public static void AddDepartment()
+        {
+            
             DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
 
+
+            //gives user ability to add a department
             Console.WriteLine("Hello user, here are the current departments:");
             Console.WriteLine("Please press enter . . .");
             Console.ReadLine();
 
             var depos = repo.GetAllDepartments();
-            Print(depos);
+            ListDepartments();
 
 
             Console.WriteLine("Do you want to add a department?");
@@ -42,19 +110,50 @@ namespace BestBuyCRUD
                 userResponse = Console.ReadLine();
 
                 repo.InsertDepartment(userResponse);
-                Print(repo.GetAllDepartments());
+                ListDepartments();
             }
 
             Console.WriteLine("Have a great day.");
+
         }
 
-        private static void Print(IEnumerable<Department> depos)
+
+
+
+        public static void ListDepartments()
         {
-            foreach (var depot in depos)
+            var repo = new DapperDepartmentRepository(conn);
+
+            var departments = repo.GetAllDepartments();
+
+            foreach (var department in departments)
             {
-                Console.WriteLine($"ID: {depot.DepartmentID} Name: {depot.Name}");
+                Console.WriteLine($"Department ID: {department.DepartmentID}      Name: {department.Name} ");
+                Console.WriteLine("                                                                  ");
             }
 
+            Console.WriteLine("------------------------------------");
+
         }
+
+
+  
+        public static void ListProducts()
+        {
+            var repo = new DapperProductRepository(conn);
+
+            var products = repo.GetAllProducts();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductID}      Name: {product.Name} ");                
+            }
+
+            Console.WriteLine("------------------------------------");
+        }
+
+
+
+
     }
 }
