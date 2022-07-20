34b529cffc3bc1391df3daa50a66de02c66176e8
BrookeAL0012 <bbcecil17@gmail.com>
2022-07-20 11:27:01 -0500

    Added insert department method, changed console write and read lines

---
+++ BestBuyCRUD/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestBuyCRUD
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments();
+        void InsertDepartment(string newDepartmentName);
+    }
+}
